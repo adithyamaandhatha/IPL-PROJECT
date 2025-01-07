8b16badc5abf58e6e7a486d1f4ebffc915f24bf0
username <wecp-dummy@gmail.com>
2025-01-07 13:20:39 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
@@ -1,5 +1,27 @@
 package com.wecp.progressive.repository;
 
+import javax.transaction.Transactional;
 
-public interface VoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.wecp.progressive.entity.Vote;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Integer>{
+
+    Long countByCategory(String category);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE v FROM Vote v WHERE v.team.teamId = :teamId")
+    void deleteByTeamId(int teamId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Vote v WHERE v.cricketer.cricketerId = :cricketerId")
+    void deleteByCricketerId(@Param("cricketerId") int cricketerId);
 }
